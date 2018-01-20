738bccf0cf2c4dd825ecade47239a849a6114a00
Stephen Vanterpool <stephen@vanterpool.net>
2018-01-19 17:11:38 -0800

    blah

--- setup.py
+++ setup.py
@@ -3,5 +3,8 @@ from setuptools import setup,find_packages
 setup(
     name = 'jvc_tools',
     version = '0.1',
-    packages=find_packages() #['jvc_tools']
+    url = 'https://github.com/blackgold9/jvc_tools',
+    author = 'Stephen Vanterpool',
+    author_email = 'stephen+jvc@vanterpool.net'
+    packages=find_packages() 
     )
